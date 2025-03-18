e74773bbc7e32bfde7d284b9d103e4150040f9ec
Junfa Li <1170640485fg@gmail.com>
2025-03-18 16:38:18 -0300

    Task 2 Finished

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,13 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn; // Get the params isbn
 
-  return res.status(300).json({ message: "Yet to be implemented" });
+  if (books[isbn]) {
+    res.status(200).json(books[isbn]);
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
 });
 
 // Get book details based on author
